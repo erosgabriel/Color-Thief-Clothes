0b3d0e21bcb560f7e808a7782d114c7aa9cd0d33
Eros Gabriel <cirilo14tavares@gmail.com>
2023-06-16 14:57:06 -0300

    Nav-link color change with scroll

--- js/inicial.js
+++ js/inicial.js
@@ -1,3 +1,28 @@
-function aviso(){
+()=>{
         alert('Olá, essa parte do site ainda está em desenvolvimento. Em breve estaremos com tudo pronto.')
 }
+
+window.addEventListener('scroll', ()=>{
+  var menu = document.querySelector('.invernobutton');
+  var link = document.querySelector('.link1');
+  link.classList.toggle('sticky', window.scrollY < 300);
+  menu.classList.toggle('sticky', window.scrollY < 300);
+})
+window.addEventListener('scroll', ()=>{
+  var menu = document.querySelector('.veraobutton');
+  var link = document.querySelector('.link2');
+  link.classList.toggle('sticky', window.scrollY > 300 && window.scrollY < 1000);
+  menu.classList.toggle('sticky', window.scrollY > 300 && window.scrollY < 1000);
+})
+window.addEventListener('scroll', ()=>{
+  var menu = document.querySelector('.outonobutton');
+  var link = document.querySelector('.link3');
+  link.classList.toggle('sticky', window.scrollY > 1000 && window.scrollY < 1600);
+  menu.classList.toggle('sticky', window.scrollY > 1000 && window.scrollY < 1600);
+})
+window.addEventListener('scroll', ()=>{
+  var menu = document.querySelector('.primaverabutton');
+  var link = document.querySelector('.link4');
+  link.classList.toggle('sticky', window.scrollY > 1600);
+  menu.classList.toggle('sticky', window.scrollY > 1600);
+})
